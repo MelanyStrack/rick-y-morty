2cda22bf3b2441d898d095258e35804b5aa9ca1a
MelanyStrack <melany.strack@gmail.com>
2024-12-13 10:22:50 -0300

    added ListCharacter component with fetch and Character Card component

---
+++ frontend/src/pages/CharacterCard.tsx
@@ -0,0 +1,38 @@
+import './App.css'
+
+export interface DataCardProps{
+    id: number,
+      name: string,
+      species: string,
+      gender: string,
+      image: string,
+}
+
+
+export function CharacterCard({ id, name, species, gender,image }: DataCardProps) {
+
+    return (
+        <a href={`/personaje/${id}`}>
+        <div
+            className="flex justify-center hover:cursor-pointer">
+            <div className="">
+                <img
+                    src={image}
+                    alt=""
+                />
+                <div className="w-full">
+                    <h2 className="">
+                        {name}
+                    </h2>
+                    <h4>
+                        {species}
+                    </h4>
+                    <h4>
+                        {gender}
+                    </h4>
+                </div>
+            </div>
+        </div>
+        </a>
+    );
+}
---
+++ frontend/src/pages/ListCharacters.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { CharacterCard } from './CharacterCard';
+
+export interface Data{
+    id: number,
+      name: string,
+      status: string,
+      species: string,
+      type: string,
+      gender: string,
+      image: string,
+}
+
+export default function ListCharacters() {
+    const [data, setData] = useState<Data[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+  
+    useEffect(() => {
+      fetch("https://rickandmortyapi.com/api/character")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Error fetching data');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setData(data.results);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+          setLoading(false);
+        });
+    }, []);
+  
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Ups... algo salió mal</p>;
+
+
+  return (
+    <>
+      <div>
+      <h1>Personajes</h1>
+        {data.map((item, i) => (
+          <CharacterCard key={item.id + i}
+          id={item.id}
+          name={item.name}
+          species={item.species}
+          gender={item.gender}
+          image={item.image}
+          ></CharacterCard>
+        ))}
+    </div>
+    </>
+  )
+}
